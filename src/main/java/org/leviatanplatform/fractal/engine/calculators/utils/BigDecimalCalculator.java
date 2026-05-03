9fc14da0ba61d299d3faa4025e1a2ac41b1e8338
CiudadAlex <ciudad.alex@gmail.com>
2026-05-03 14:00:36 +0200

    Fractal

---
+++ src/main/java/org/leviatanplatform/fractal/engine/calculators/utils/BigDecimalCalculator.java
@@ -0,0 +1,29 @@
+package org.leviatanplatform.fractal.engine.calculators.utils;
+
+import java.math.BigDecimal;
+
+public class BigDecimalCalculator {
+
+    public static BigDecimal getModulus(BigDecimal real, BigDecimal imag) {
+
+        BigDecimal real2 = multiply(real, real);
+        BigDecimal imag2 = multiply(imag, imag);
+
+        return real2.add(imag2);
+    }
+
+    public static BigDecimal multiply(int i, BigDecimal d) {
+        return multiply(d, BigDecimal.valueOf(i));
+    }
+
+    public static BigDecimal multiply(BigDecimal d1, BigDecimal d2, BigDecimal d3) {
+        return multiply(multiply(d1, d2), d3);
+    }
+
+    public static BigDecimal multiply(BigDecimal d1, BigDecimal d2) {
+
+        // FIXME truncate
+        //BigDecimal truncado = valor.setScale(2, RoundingMode.DOWN);
+        return d1.multiply(d2);
+    }
+}
--- src/main/java/org/leviatanplatform/fractal/engine/calculators/utils/JuliaUtils.java
+++ src/main/java/org/leviatanplatform/fractal/engine/calculators/utils/JuliaUtils.java
@@ -50,16 +50,16 @@ public class JuliaUtils {
 
         for (int i = 0; i < iterations; i++) {
 
-            BigDecimal zre2 = BigDecimalUtils.multiply(zre, zre);
-            BigDecimal zim2 = BigDecimalUtils.multiply(zim, zim);
+            BigDecimal zre2 = BigDecimalCalculator.multiply(zre, zre);
+            BigDecimal zim2 = BigDecimalCalculator.multiply(zim, zim);
 
             BigDecimal z2re = zre2.subtract(zim2);
-            BigDecimal z2im = BigDecimalUtils.multiply(VALUE_2, zre, zim);
+            BigDecimal z2im = BigDecimalCalculator.multiply(VALUE_2, zre, zim);
 
             zre = z2re.add(c_real);
             zim = z2im.add(c_imaginary);
 
-            BigDecimal modulus = BigDecimalUtils.getModulus(zre, zim);
+            BigDecimal modulus = BigDecimalCalculator.getModulus(zre, zim);
 
             if (modulus.compareTo(VALUE_10) > 0) {
                 // If exits return iteration
