364aa7e080f976f4ac6725d085659a97a8dde56e
CiudadAlex <ciudad.alex@gmail.com>
2026-05-03 14:37:29 +0200

    Fractal

--- src/main/java/org/leviatanplatform/fractal/engine/ComplexPlane.java
+++ src/main/java/org/leviatanplatform/fractal/engine/ComplexPlane.java
@@ -67,13 +67,15 @@ public class ComplexPlane {
 
     public void calculatePrecision(Calculator calculator) {
 
+        BigDecimalCalculator bigDecimalCalculator = BigDecimalCalculator.get();
+
         TicToc ticToc = new TicToc();
 
         for (int r = 0; r < mumber_steps_real; r++) {
             for (int i = 0; i < mumber_steps_imag; i++) {
 
-                BigDecimal real = bd_real_min.add(BigDecimalCalculator.get().multiply(r, bd_step));
-                BigDecimal imaginary = bd_imag_min.add(BigDecimalCalculator.get().multiply(i, bd_step));
+                BigDecimal real = bd_real_min.add(bigDecimalCalculator.multiply(r, bd_step));
+                BigDecimal imaginary = bd_imag_min.add(bigDecimalCalculator.multiply(i, bd_step));
 
                 plane[r][i] = calculator.calculate(real, imaginary);
             }
